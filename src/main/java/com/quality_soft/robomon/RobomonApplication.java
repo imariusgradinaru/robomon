c67e991ef1d2baa855a65e376065cce619cf5852
Marius Gradinaru <44640232+imariusgradinaru@users.noreply.github.com>
2020-08-17 17:42:25 +0200

    Add files via upload

---
+++ src/main/java/com/quality_soft/robomon/RobomonApplication.java
@@ -0,0 +1,13 @@
+package com.quality_soft.robomon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobomonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobomonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quality_soft/robomon/RobomonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quality_soft.robomon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobomonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
